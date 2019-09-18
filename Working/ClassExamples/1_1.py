d1b98c1834f478707ce8fe5d6723194fc0a587a1
Ian132496 <sungkyun.cho22@ycdsbk12.ca>
2019-09-18 09:38:57 -0400

    1_1_1_printing.py

---
+++ Working/ClassExamples/1_1.py
@@ -0,0 +1,11 @@
+print("This is a print statement")
+print("What's wrong with this statement?")
+print("How about this one?")
+print(5)
+print("here are the ages of my three children 12, 7, 4")
+print(3.14,"is my favourite pi")
+my_number = 50
+print(my_number)
+length = 14 
+width = 1234   
+print("my yard is", length, "by", width)
---
+++ Working/practice questions/1_1_1_printing.py
@@ -0,0 +1,21 @@
+print("my name is Ian.")
+print("I like writing Python programs!")
+print("Hi there")
+print("My favourite colour is blue")
+print("(\____/)")
+print(" \ ** /")
+print("  \  /")
+print ("    o")
+print("Keisha")
+print("10")
+a = 10
+c = 20
+b = c
+d = a + b
+f = 50
+e = f +z d
+print (a)
+print (b)
+print (c)
+print (d)
+print (e)
